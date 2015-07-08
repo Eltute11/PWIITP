06487392b8e40f17c5ccef3ea9bc003a4e826b2d
jurcola <juanig.urcola@gmail.com>
2015-07-07 23:17:20 -0300

    Cliente.php
    Agrego pagina login del cliente.

---
+++ aside_cliente.php
@@ -0,0 +1,79 @@
+<!-- aside -->
+ <aside id="aside" class="app-aside hidden-xs bg-dark">
+          <div class="aside-wrap">
+        <div class="navi-wrap">
+          <!-- user -->
+          <!-- nav -->
+          <nav ui-nav class="navi clearfix">
+            <ul class="nav">
+              <!-- <li class="hidden-folded padder m-t m-b-sm text-muted text-xs">
+                <span>Navigation</span>
+              </li> -->
+              <li>
+                <!-- <a href="profile.php"> -->
+                  <a href="cliente.php"> 
+                  <i class="icon-user icon text-success-lter"></i>
+                  <span>Perfil</span>
+                </a>
+              </li>
+              <li>
+                <!-- <a href="profile.php"> -->
+                  <a href="php/cerrarSesion.php"> 
+                  <i class="icon-logout icon text-success-lter"></i>
+                  <span>Cerrar Sesion</span>
+                </a>
+              </li>
+              
+              <li class="line dk"></li>
+      
+              <li>
+                  <a href='#' class="auto">      
+                  <i class="icon-camcorder"></i>
+                  <span>Cámaras</span>
+                </a>
+              </li>
+
+              <li>
+                  <a href='#' class="auto">      
+                  <i class="icon-volume-2"></i>
+                  <span>Disparar Alarma</span>
+                </a>
+              </li>
+
+
+              <li>
+                  <a href='#' class="auto">      
+                  <i class="icon-call-out"></i>
+                  <span>911</span>
+                </a>
+              </li>
+
+
+
+              <li>
+                <a href="#">
+                  <i class="icon-lock-open"></i>
+                  <span>Desbloqueo QR</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="pull-right text-muted">
+                    <i class="fa fa-fw fa-angle-right text"></i>
+                    <i class="fa fa-fw fa-angle-down text-active"></i>
+                  </span>
+                  <i class="glyphicon glyphicon-file icon"></i>
+                  <span>Facturas</span>
+                </a>
+              </li>
+
+              <li class="line dk hidden-folded"></li>
+
+              
+            </ul>
+          </nav>
+          <!-- nav -->
+        </div>
+      </div>
+  </aside>
+<!-- / aside -->
