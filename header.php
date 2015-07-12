92d1dc3af4a47829417dcfc2173223ba0b6c63d6
Matias Araus <matias_araus@hotmail.com>
2015-07-12 11:05:26 -0300

    Agregar dato a sesion
    Se a modificado, para que se almacene el ID de perfil para luego poder
    ser utilizado como identificador en una consulta para el codigo de
    alarma

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <link rel="icon" type="image/png" href="img/icon.png" />
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css" type="text/css" />
 <!--  <link rel="stylesheet" href="bower_components/animate.css/animate.css" type="text/css" /> -->
-<!--  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css" type="text/css" />-->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css" type="text/css" />
   <link rel="stylesheet" href="bower_components/simple-line-icons/css/simple-line-icons.css" type="text/css" /> 
   <link rel="stylesheet" href="css/font.css" type="text/css" />
   <link rel="stylesheet" href="css/app.css" type="text/css" />
